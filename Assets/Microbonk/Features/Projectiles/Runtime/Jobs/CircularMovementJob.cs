aeda63e8cd7bec10de7b7f7b7453400c049cf938
Dawid Darłak <primsh@o2.pl>
2025-10-17 23:06:50 +0200

    separate jobs for movement and spawning

---
+++ Assets/Microbonk/Features/Projectiles/Runtime/Jobs/CircularMovementJob.cs
@@ -0,0 +1,40 @@
+using Microbonk.Features.Projectiles.Runtime.Components.ProjectileSpawning;
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+namespace Microbonk.Features.Projectiles.Runtime.Systems
+{
+    [BurstCompile]
+    public partial struct CircularMovementJob : IJobEntity
+    {
+        public double ElapsedTime;
+
+        [ReadOnly]
+        public ComponentLookup<LocalToWorld> OriginPositions;
+
+        private void Execute(ref LocalTransform spawnerTransform, in CircularMovementAroundTransform spawner)
+        {
+            var speed = spawner.AngularSpeed;
+            var radius = spawner.Radius;
+            var origin = this.OriginPositions[spawner.Origin].Position;
+            var angle = (float)this.ElapsedTime * speed;
+
+            var position = new float3(
+                origin.x + math.sin(angle) * radius,
+                origin.y,
+                origin.z + math.cos(angle) * radius
+            );
+
+            var direction = new float3(
+                math.cos(angle - math.PIHALF),
+                0,
+                math.sin(angle + math.PIHALF));
+            var rotation = quaternion.LookRotation(direction, math.up());
+            
+            spawnerTransform = LocalTransform.FromPositionRotation(position, rotation);
+        }
+    }
+}
---
+++ Assets/Microbonk/Features/Projectiles/Runtime/Systems/Spawning/ProjectileSpawnerCircularMovementSystem.cs
@@ -0,0 +1,26 @@
+using Microbonk.Features.Projectiles.Runtime.Components.ProjectileSpawning;
+using Unity.Burst;
+using Unity.Entities;
+using Unity.Transforms;
+
+namespace Microbonk.Features.Projectiles.Runtime.Systems
+{
+    [BurstCompile]
+    [UpdateInGroup(typeof(FixedStepSimulationSystemGroup))]
+    public partial struct ProjectileSpawnerCircularMovementSystem : ISystem
+    {
+        public void OnCreate(ref SystemState state)
+        {
+            state.RequireForUpdate<CircularMovementAroundTransform>();
+        }
+
+        public void OnUpdate(ref SystemState state)
+        {
+            new CircularMovementJob
+            {
+                ElapsedTime = SystemAPI.Time.ElapsedTime,
+                OriginPositions = SystemAPI.GetComponentLookup<LocalToWorld>(true)
+            }.ScheduleParallel();
+        }
+    }
+}
